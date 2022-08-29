2327aeffb46a043c71c0721bc7b8903ff477c889
Eren Kaplan <ek.codeproject@gmail.com>
2022-08-29 16:00:28 +0300

    keybase claim

---
+++ pages/keybase.txt.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+export default class KeybaseTxt extends Component {
+  static getInitialProps ({ res }) {
+    res.end(claim)
+  }
+
+  render () {
+    // eslint-disable-next-line react/no-unescaped-entities
+    return <div> We'll never get this far</div>
+  }
+}
+
+const claim = `==================================================================
+https://keybase.io/erenkaplan
+--------------------------------------------------------------------
+
+I hereby claim:
+
+  * I am an admin of https://pepegapepe.ga
+  * I am erenkaplan (https://keybase.io/erenkaplan) on keybase.
+  * I have a public key ASCadRdELRA2IdwbZyXagqgZ1eokK8Ck6zyDDj2mWxKYbwo
+
+To do so, I am signing this object:
+
+{
+  "body": {
+    "key": {
+      "eldest_kid": "012078bb4700a62e50b54118c8fc5686bbcb92215f55933014205db770d95c81040d0a",
+      "host": "keybase.io",
+      "kid": "01209a7517442d103621dc1b6725da82a819d5ea242bc0a4eb3c830e3da65b12986f0a",
+      "uid": "163e1da3bd9db85f4d036ba2600c1919",
+      "username": "erenkaplan"
+    },
+    "merkle_root": {
+      "ctime": 1661777897,
+      "hash": "95a81661f97ff537b616ef21cec23e346be9ed64498b03bdc1acea0152bae4f738b0192fd51f0d3092898d518bc353ff14640ecf1f4868318a613657c78eb190",
+      "hash_meta": "a8a810a67e0d7ce4a3ce8fb8c443afeb27c4d9eae28d4502d999969a626febdf",
+      "seqno": 23104509
+    },
+    "service": {
+      "entropy": "rNWrQa9BFVoKZJkhRV3lYWWW",
+      "hostname": "pepegapepe.ga",
+      "protocol": "https:"
+    },
+    "type": "web_service_binding",
+    "version": 2
+  },
+  "client": {
+    "name": "keybase.io go client",
+    "version": "6.0.1"
+  },
+  "ctime": 1661777926,
+  "expire_in": 504576000,
+  "prev": "fb6c41e71d37ac8238515d6d709b2092cfcd93e406aae803ef9c60647fb731de",
+  "seqno": 398,
+  "tag": "signature"
+}
+
+which yields the signature:
+
+hKRib2R5hqhkZXRhY2hlZMOpaGFzaF90eXBlCqNrZXnEIwEgmnUXRC0QNiHcG2cl2oKoGdXqJCvApOs8gw49plsSmG8Kp3BheWxvYWTETJcCzQGOxCD7bEHnHTesgjhRXW1wmyCSz82T5Aaq6APvnGBkf7cx3sQg17WYJlAF8Lu0hvR3iT6ZFIh9DBUptUz/B0sy98UlwnACAcKjc2lnxEDi4R9DSNa4A70bZER6+zEn1zFaU9OqIBiZTuGsNnYfGtz9iXd/pbU3/peKhjzw7PvsQy6HmTCCAGm146sGSgULqHNpZ190eXBlIKRoYXNogqR0eXBlCKV2YWx1ZcQg+WesqvRSzSuz9Hga6b9oCo+hpk/uwozPBlpvO17JZJqjdGFnzQICp3ZlcnNpb24B
+
+And finally, I am proving ownership of this host by posting or
+appending to this document.
+
+View my publicly-auditable identity here: https://keybase.io/erenkaplan
+
+==================================================================`
